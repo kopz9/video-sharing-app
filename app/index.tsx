7325d9adebb23da1b290a817a5ebd2e9470bf00e
kopz <gustavoeduardo438@gmail.com>
2024-05-19 04:59:55 -0300

    feat: add nativewind + assets folder

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function RootLayout() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl font-pbold">Hello</Text>
+    </View>
+  );
+}
